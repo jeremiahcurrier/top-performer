71a56f3936763066ac8933c3ae4d85271cc681e2
Jeremiah Currier <jcurrier@zendesk.com>
2014-09-25 16:12:25 +0100

    adding files

---
+++ app.js
@@ -0,0 +1,55 @@
+(function() {
+
+  function asTwoDigits(number) { // formats number to a two digit output
+    number = '' + number;
+    if (number.length === 1) { number = '0' + number; } // starts number at 1 instead of 0
+    return number;
+  }
+
+  function todayFormatted() { // formats the date to use in search API call
+    var now = new Date(),
+        month = now.getMonth() + 1;
+    return now.getFullYear() + '-' + asTwoDigits(month) + '-' + asTwoDigits(now.getDate());
+  }
+
+  return {
+    events: {
+      'app.activated': 'start', // initialize 'start' function that calls the 'solvedTodayByMe' function
+      'solvedTodayByMe.done': 'renderCountMe' // when 'solvedTodayByMe' is finished do 'renderCountMe' function
+    },
+
+    requests: { // AJAX Query using search API for all tickets solved by currently logged in agent today
+      solvedTodayByMe: function() {
+        return {
+          url: '/api/v2/search.json?query=assignee:me+status:solved+solved:' + todayFormatted(),
+          dataType: 'json',
+          type: 'GET'
+        };
+      }
+    },
+
+    start: function() {
+      this.ajax('solvedTodayByMe');
+    },
+
+    renderCountMe: function(searchResults) {
+      var count = searchResults.count; // counts value returned in array from search API query
+      if (count <= 2) {
+        this.switchTo('show', { count: count, text: "slacker...", color: "red" });
+      } else if (count >= 3  && count <= 10 ) {
+        this.switchTo('show', { count: count, text: "keep going...", color: "yellow" });
+      } else {
+        this.switchTo('show', { count: count, text: "great job!", color: "green" });
+      }      
+  }
+};
+
+}());
+
+
+
+
+
+
+
+    
